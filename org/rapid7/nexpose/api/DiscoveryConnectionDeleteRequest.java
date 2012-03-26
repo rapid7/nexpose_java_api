cefa792050e135d085eec2ecd5d9904c7b0d9024
unknown <mrongali@lat-e4300-0008.tor.rapid7.com>
2012-03-26 12:55:04 -0700

    Added Discovery connection API support to nexpose_java_api

---
+++ org/rapid7/nexpose/api/DiscoveryConnectionDeleteRequest.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (C) 2012, Rapid7 LLC, Boston, MA, USA.
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * * Neither the name of the <organization> nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.rapid7.nexpose.api;
+
+import org.rapid7.nexpose.api.APISession.APISupportedVersion;
+
+/**
+ * Encapsulates the {@link DiscoveryConnectionDeleteRequest} Nexpose API request.
+ *
+ * @author Murali Rongali
+ */
+public class DiscoveryConnectionDeleteRequest extends TemplateAPIRequest
+{
+   /////////////////////////////////////////////////////////////////////////
+   // Public methods
+   /////////////////////////////////////////////////////////////////////////
+
+   /**
+    * Constructs a discovery connection delete request with its associated API version
+    * information.
+    *
+    * @param sessionId the session to submit the request with. May not be {@code null} nor empty and must
+    * be a 40 character hex {@link String}.
+    * @param syncId the sync id to identify the response. May be {@code null}.
+    * @param connectionID the id of the DiscoveryConnection.
+    */
+   public DiscoveryConnectionDeleteRequest(String sessionId, String syncId, String connectionID)
+   {
+      super(sessionId, syncId);
+      set("connectionID", connectionID);
+      m_firstSupportedVersion = APISupportedVersion.V1_2;
+      m_lastSupportedVersion = APISupportedVersion.V1_2;
+   }
+}
